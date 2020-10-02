bdb5ae240742723cc2f6c61ee71da720ee7b24d0
Alan Morales <alan_jesus99@hotmail.com>
2020-10-01 23:19:12 -0500

    implementing save method

--- src/main/java/dao/TransactionInfoMongo.java
+++ src/main/java/dao/TransactionInfoMongo.java
@@ -1,10 +1,42 @@
 package dao;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import config.MongoConfig;
+import org.bson.Document;
 import transactionInfo.ITransactionInfo;
 
 public class TransactionInfoMongo implements TransactionInfoDao{
+    private final MongoDatabase mongo;
+    private final String collectionName = "transactions";
+
+    public TransactionInfoMongo() {
+        ConnectionString connString = new ConnectionString(MongoConfig.MONGO_URI);
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connString)
+                .retryWrites(true)
+                .build();
+        MongoClient mongoClient = MongoClients.create(settings);
+        mongo = mongoClient.getDatabase("gradle-java-test");
+    }
+
     @Override
     public boolean save(ITransactionInfo transactionInfo) {
-        return false;
+        try {
+            Document doc = new Document("clientName", transactionInfo.getClientName())
+                    .append("id", transactionInfo.getTransactionId())
+                    .append("date", transactionInfo.getDate())
+                    .append("concept", transactionInfo.getPaymentConcept())
+                    .append("amount", transactionInfo.getAmount());
+
+            mongo.getCollection(collectionName).insertOne(doc);
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
     }
 }
