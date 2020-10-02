10012cae01a3ede2117eb5156f31d0f391f85b12
Alan Morales <alan_jesus99@hotmail.com>
2020-10-01 21:11:24 -0500

    added methods

--- src/main/java/transactionInfo/ITransactionInfo.java
+++ src/main/java/transactionInfo/ITransactionInfo.java
@@ -1,4 +1,11 @@
 package transactionInfo;
 
+import java.util.Date;
+
 public interface ITransactionInfo {
+    void setTransactionId(String id);
+    void setAmount(float amount);
+    void setDate(Date date);
+    void setClientName(String name);
+    void setPaymentConcept(String concept);
 }
