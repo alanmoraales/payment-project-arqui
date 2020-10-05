47ad9ce801855fe427a26b363866ea854c309f48
MCAlberto55 <47836982+MCAlberto55@users.noreply.github.com>
2020-10-04 22:27:42 -0500

    Add files via upload

---
+++ src/main/java/view/PayViewController.java
@@ -0,0 +1,39 @@
+package view;
+
+import paymentController.PaymentController;
+
+public class PayViewController {
+
+    private PaymentController controller;
+    private PayView view;
+
+    public PayViewController(PaymentController controller) {
+        view = new PayView(this);
+        view.setVisible(true);
+        this.controller = controller;
+    }
+    public void setMethod(String method){
+        controller.setPaymentMethod(method);
+    }
+    public void creaditCardNumer(String number){
+        controller.addCreditCardNumber(number);
+    }
+    public void ownerName(String name){
+        controller.addCreditCardOwnerName(name);
+    }
+    public void month(int month){
+        controller.addCreditCardExpirationMonth(month);
+    }
+    public void year(int year){
+        controller.addCreditCardExpirationYear(year);
+    }
+    public void cvc(int cvc){
+        controller.addCreditCardCVC(cvc);
+    }
+    public void phoneNumber(long phoneNumber){
+        controller.addUserPhoneNumber(phoneNumber);
+    }
+    public void pay(double amount){
+        controller.pay(amount);
+    }
+}
